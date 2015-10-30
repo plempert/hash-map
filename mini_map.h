6490edfe7eacaf4b16169f05ae5787d8ad3b76b8
Patrick <pat.lempert@gmail.com>
2015-10-29 22:29:23 -0400

    Fix hash function and add comments

--- mini_map.h
+++ mini_map.h
@@ -2,6 +2,7 @@
 #define MINI_MAP
 
 #include "mini_list.h"
+#include <iostream>
 
 template <typename T>
 class MiniMap{
@@ -20,7 +21,7 @@ private:
     //https://en.wikipedia.org/wiki/Jenkins_hash_function
     int hash_function(const char* key){
         unsigned long hash, i;
-        for(hash = i = 0; key[i] == 0; i++){
+        for(hash = i = 0; key[i] != 0; i++){
             hash += key[i];
             hash += (hash << 10);
             hash ^= (hash >> 6);
@@ -28,13 +29,18 @@ private:
         hash += (hash << 3);
         hash ^= (hash >> 11);
         hash += (hash << 15);
-        return hash;
+        //hash is now almost 20 digits long,
+        //so I took the mod operator to 
+        //get a valid index
+        return hash % size;
     }
 
 public:
     //Initialize the size and set 
     //each pointer pointing to nullptr
     MiniMap(int size):size(size){
+        //A little input validation
+        if( !(size > 0) ) size = 100;
         hash_table = new Node<T>* [size];
         for(int i = 0; i < size; i++){
             hash_table[i] = nullptr;
@@ -45,10 +51,17 @@ public:
         delete hash_table;
     }
     bool set(const char* key, T value){
+        //Specs ask for a fixed size hash map, but
+        //with collision handling this implementation
+        //can support a variable-sized hash map.
+        //Nevertheless we check to make sure 
+        //num_of_items does not exceed size; 
         if (num_of_items == size) return false;
         int hash_value = hash_function(key);
         Node<T> *inserted_value = new Node<T>(key, new T(value), hash_table[hash_value], nullptr);
+        //Check for collision and adjust prev pointer on following element to point to inserted element
         if(inserted_value->next != nullptr) inserted_value->next->prev = inserted_value;
+        //Have the hash table point to the newly-inserted element
         hash_table[hash_value] = inserted_value;
         num_of_items++;
         return true;
@@ -70,13 +83,20 @@ public:
         while(ptr != nullptr){
             if(key == ptr->key){
                 T* return_value = ptr->value;
+                //There are three cases for removing an element:
+                //the element could be the first in the list,
+                //the last in the list, or somewhere in the middle.
+                //Here we cover all cases.
+                //Element is first in the list: 
                 if(ptr->prev == nullptr){
                     hash_table[hash_value] = ptr->next;
                     if(ptr->next != nullptr) ptr->next->prev = nullptr;
                 }
+                //Element is last in the list:
                 if(ptr->next == nullptr){
                     if(ptr->prev != nullptr) ptr->prev->next = nullptr;
                 }
+                //Element is somewhere in between:
                 if(ptr->prev != nullptr && ptr->next != nullptr){
                     ptr->prev->next = ptr->next;
                     ptr->next->prev = ptr->prev;
